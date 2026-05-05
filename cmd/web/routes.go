5c898cb2a0adba906b713b7d6694846f21d01a94
Le Tran Quang Huy (MS/EDS42-XC) <Huy.LeTranQuang@vn.bosch.com>
2026-05-05 16:27:28 +0700

    page 205

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"strings"
+
+	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
@@ -23,7 +25,12 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	return app.logRequest(commonHeaders(mux))
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+
+	// Return the 'standard' middleware chain followed by the servemux
+	return standard.Then(mux)
 }
 
 // neuter is a middleware that prevents directory listings by returning a 404
